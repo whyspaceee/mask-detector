7d4ce5f7e1e5d247c343722bc77d6c63b93d06ef
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2022-11-26 22:35:01 +0700

    added new models that use sobel filters

--- test.py
+++ test.py
@@ -2,13 +2,16 @@ import cv2 as cv
 import os
 import pickle
 from sklearn.svm import SVC
-from skimage.filters import gabor
+from skimage.filters import rank, sobel
 import numpy as np
+from skimage.morphology import disk
+from skimage.feature import hog, local_binary_pattern, ORB
+
 
 cv2_base_dir = os.path.dirname(os.path.abspath(cv.__file__))
 haar_model = os.path.join(
     cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
-svm = pickle.load(open('model9.sav', 'rb'))
+svm = pickle.load(open('model10.sav', 'rb'))
 haar_classifier = cv.CascadeClassifier(haar_model)
 names = {0: 'Mask', 1: 'Without Mask'}
 
@@ -24,6 +27,21 @@ def kmeans(img):
     res = center[label.flatten()]
     res2 = res.reshape((img.shape))
     return res2
+
+def describe(image, eps=1e-7):
+		# compute the Local Binary Pattern representation
+		# of the image, and then use the LBP representation
+		# to build the histogram of patterns
+		lbp = local_binary_pattern(image, 24,
+			8, method="uniform")
+		(hist, _) = np.histogram(lbp.ravel(),
+			bins=np.arange(0, 24 + 3),
+			range=(0, 24 + 2))
+		# normalize the histogram
+		hist = hist.astype("float")
+		hist /= (hist.sum() + eps)
+		# return the histogram of Local Binary Patterns
+		return hist
 
 def gabor_filter(img):
     return gabor(img, frequency=0.8)[0]
@@ -45,13 +63,21 @@ if capture.isOpened():
                 cv.rectangle(img, (x,y), (x+w, y+h), (122,50,200),2)
                 face = img[y:y+h, x:x+w]
                 face = cv.resize(face, (50, 50))
-                face = gabor_filter(face)
                 face = kmeans(face)
-                face = face.reshape(1,-1)
-                pred = svm.predict(face)[0]
+                face = rank.mean_bilateral(
+                    face, footprint=disk(2), s0=50, s1=50
+                )
+                edges = np.array(sobel(face))
+                hist = np.array(describe(face))
+                edgesShaped = edges.reshape(2500)
+                conc = np.concatenate((edgesShaped,hist), axis =0)
+                conc = conc.reshape(1,-1)
+                pred = svm.predict(conc)[0]
                 n = names[int(pred)]
                 print(n)
             cv.imshow('test',img)
+
+
         if cv.waitKey(2) == 27:
             break
 
